d5e992744b8a2af979db0b33c1fa66d670286c3d
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-11-02 15:36:46 -0300

    create: controllers

---
+++ backend/src/controllers/loginController.js
@@ -0,0 +1,23 @@
+const loginService = require('../service/loginService');
+
+const createToken = async (req, res) => {
+  console.log(req.body)
+  try {
+    const { email, password } = req.body;
+    const token = await loginService.createNewToken(email, password);
+    if (token.erroCode) { 
+      return res.status(401)
+        .json({
+            message: token.erroCode,
+        });
+    }
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Aconteceu erro realizar login.' });
+  }
+};
+
+module.exports = {
+  createToken,
+};
